251e419df9d708983b7330c8ed7411b524648069
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-10-28 12:41:14 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,29 +1,45 @@
-function love.load()
-    love.window.setMode(320, 480, {resizable = false})
-    background = love.graphics.newImage("imagens/background.png")
-    x, y, w, h = 20, 20, 60, 20
-end
+aviao_14bis = {
+    src = "imagens/14bis.png",
+    largura = 64,
+    altura = 64,
+    x = 0,
+    y = 0
+}
 
-function love.update(dt)
+function move_14bis()
     if love.keyboard.isDown('up')then
-        y = y - 2
+        aviao_14bis.y = aviao_14bis.y - 2
     end
 
     if love.keyboard.isDown('down')then
-        y = y + 2
+        aviao_14bis.y = aviao_14bis.y + 2
     end
 
     if love.keyboard.isDown('left')then
-        x = x - 2
+        aviao_14bis.x = aviao_14bis.x - 2
     end
 
     if love.keyboard.isDown('right')then
-        x = x + 2
+        aviao_14bis.x = aviao_14bis.x + 2
     end
+end
 
+function love.load()
+    love.window.setMode(320, 480, {resizable = false})
+    love.window.setTitle("14-Bis vs Meteoros")
+    love.graphics.newImage(aviao_14bis.src)
+    background = love.graphics.newImage("imagens/background.png")
+    x, y, w, h = 20, 20, 60, 20
+    aviao_14bis.imagem = love.graphics
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('up','down','left','right') then
+
+    end
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-    love.graphics.rectangle("fill", x, y, w, h)
+    love.graphics.draw(aviao_14bis.imagem)
 end
