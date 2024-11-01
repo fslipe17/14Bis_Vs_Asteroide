252212ca793041d189e5ffe9a78906faa81c57f2
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-11-01 10:44:30 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,45 +1,85 @@
+LARGURA_TELA = 320
+ALTURA_TELA = 480
+MAX_METEOROS = 12
+
 aviao_14bis = {
     src = "imagens/14bis.png",
     largura = 64,
     altura = 64,
-    x = 0,
-    y = 0
+    x = LARGURA_TELA/2 - 64/2,
+    y = ALTURA_TELA - 64/2
 }
 
-function move_14bis()
-    if love.keyboard.isDown('up')then
-        aviao_14bis.y = aviao_14bis.y - 2
-    end
+meteoros = {}
 
-    if love.keyboard.isDown('down')then
-        aviao_14bis.y = aviao_14bis.y + 2
-    end
-
-    if love.keyboard.isDown('left')then
-        aviao_14bis.x = aviao_14bis.x - 2
-    end
-
-    if love.keyboard.isDown('right')then
-        aviao_14bis.x = aviao_14bis.x + 2
+function removeMeteoros()
+    for i = #meteoros, 1, -1 do
+        if meteoros[i].y > ALTURA_TELA then
+            table.remove(meteoros, i)
+        end
     end
 end
 
+function criaMeteoro()
+    meteoro = {
+        x = math.random(LARGURA_TELA),
+        y = -70,
+        peso = math.random(3),
+        deslocamento_horizontal = math.random(-1, 1)
+    }
+    table.insert(meteoros, meteoro)
+end
+
+function moveMeteoros()
+    for k, meteoro in pairs(meteoros) do
+        meteoro.y = meteoro.y + meteoro.peso
+        meteoro.x = meteoro.x + meteoro.deslocamento_horizontal
+    end
+end
+
+function move14bis()
+    if love.keyboard.isDown('w') then
+        aviao_14bis.y = aviao_14bis.y - 1
+    end
+    if love.keyboard.isDown('s') then
+        aviao_14bis.y = aviao_14bis.y + 1
+    end
+    if love.keyboard.isDown('a') then
+        aviao_14bis.x = aviao_14bis.x - 1
+    end
+    if love.keyboard.isDown('d') then
+        aviao_14bis.x = aviao_14bis.x + 1
+    end
+end    
+
 function love.load()
-    love.window.setMode(320, 480, {resizable = false})
-    love.window.setTitle("14-Bis vs Meteoros")
-    love.graphics.newImage(aviao_14bis.src)
+    love.window.setMode(LARGURA_TELA, ALTURA_TELA, {resizable = false})
+    love.window.setTitle("14bis vs Meteoros")
+
+    math.randomseed(os.time())
+
     background = love.graphics.newImage("imagens/background.png")
-    x, y, w, h = 20, 20, 60, 20
-    aviao_14bis.imagem = love.graphics
+    aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
+    meteoro_img = love.graphics.newImage("imagens/meteoro.png")
 end
 
 function love.update(dt)
-    if love.keyboard.isDown('up','down','left','right') then
-
+    if love.keyboard.isDown('w', 'a', 's', 'd') then
+        move14bis()
     end
+
+    removeMeteoros()
+    if #meteoros < MAX_METEOROS then
+        criaMeteoro()
+    end
+    moveMeteoros()
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(aviao_14bis.imagem)
+    love.graphics.draw(aviao_14bis.imagem, aviao_14bis.x, aviao_14bis.y)
+    
+    for k, meteoro in pairs(meteoros) do
+        love.graphics.draw(meteoro_img, meteoro.x, meteoro.y) 
+    end
 end
