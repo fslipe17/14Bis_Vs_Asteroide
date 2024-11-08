05b14332500298cfabbd398358e1e44a37a8476a
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2024-11-08 11:23:39 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,16 +1,26 @@
 LARGURA_TELA = 320
 ALTURA_TELA = 480
 MAX_METEOROS = 12
+FIM_JOGO = false
+
+
 
 aviao_14bis = {
     src = "imagens/14bis.png",
-    largura = 64,
-    altura = 64,
+    largura = 55,
+    altura = 61,
     x = LARGURA_TELA/2 - 64/2,
     y = ALTURA_TELA - 64/2
 }
 
 meteoros = {}
+
+function destroiAviao() 
+    aviao_14bis.src = "imagens/explosao_nave.png"
+    aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
+    aviao_14bis.largura = 67
+    aviao_14bis.altura = 77
+end
 
 function removeMeteoros()
     for i = #meteoros, 1, -1 do
@@ -24,6 +34,8 @@ function criaMeteoro()
     meteoro = {
         x = math.random(LARGURA_TELA),
         y = -70,
+        alturaM = 44,
+        larguraM = 50,
         peso = math.random(3),
         deslocamento_horizontal = math.random(-1, 1)
     }
@@ -38,19 +50,29 @@ function moveMeteoros()
 end
 
 function move14bis()
-    if love.keyboard.isDown('up') then
+    if love.keyboard.isDown('w') then
         aviao_14bis.y = aviao_14bis.y - 2
     end
-    if love.keyboard.isDown('down') then
+    if love.keyboard.isDown('s') then
         aviao_14bis.y = aviao_14bis.y + 2
     end
-    if love.keyboard.isDown('left') then
+    if love.keyboard.isDown('a') then
         aviao_14bis.x = aviao_14bis.x - 2
     end
-    if love.keyboard.isDown('right') then
+    if love.keyboard.isDown('d') then
         aviao_14bis.x = aviao_14bis.x + 2
     end
 end    
+
+function checaColisoes() 
+    for k, meteoro in pairs(meteoros) do
+        if (temColisao(meteoro.x, meteoro.y, meteoro.alturaM, meteoro.larguraM,
+        aviao_14bis.x, aviao_14bis.y, aviao_14bis.altura, aviao_14bis.largura)) then
+        destroiAviao()
+        FIM_JOGO = true
+        end
+    end
+end
 
 function love.load()
     love.window.setMode(LARGURA_TELA, ALTURA_TELA, {resizable = false})
@@ -64,15 +86,21 @@ function love.load()
 end
 
 function love.update(dt)
-    if love.keyboard.isDown('up', 'left', 'down', 'right') then
-        move14bis()
-    end
+    if not FIM_JOGO then
 
-    removeMeteoros()
-    if #meteoros < MAX_METEOROS then
-        criaMeteoro()
+        if love.keyboard.isDown('w', 'a', 's', 'd') then
+            move14bis()
+        end
+
+        removeMeteoros()
+
+        if #meteoros < MAX_METEOROS then
+            criaMeteoro()
+        end
+
+        moveMeteoros()
+        checaColisoes()
     end
-    moveMeteoros()
 end
 
 function love.draw()
@@ -82,4 +110,12 @@ function love.draw()
     for k, meteoro in pairs(meteoros) do
         love.graphics.draw(meteoro_img, meteoro.x, meteoro.y) 
     end
+end
+
+-- verificando a colisão
+function temColisao(X1, Y1, A1, L1, X2, Y2, L2, A2)
+    return  X2 < X1 + L1 and
+            X1 < X2 + L2 and
+            Y1 < Y2 + A1 and
+            Y2 < Y1 + A1 
 end
